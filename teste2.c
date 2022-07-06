b46fdaf4089df4656cc83b90e5821706566834ac
Vitor Marcondes <vitorsergio111@gmail.com>
2022-07-06 18:43:38 -0300

    transfer between files

--- teste2.c
+++ teste2.c
@@ -1,30 +1,38 @@
 #include <fcntl.h>
+#include <sys/fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {
-	// int fdinfile = open ("infile.txt", O_WRONLY);
-	// int fdoutfile = open ("outfile.txt", O_RDONLY);
-
+	int fdinfile = open("infile.txt", O_RDONLY, 0777);
+	int fdoutfile = open("outfile.txt", O_WRONLY, 0777);
 	int fd[2];
 	pipe(fd);
-	int id = fork();
+	int pid = fork();
 	
-	if (id == 0)
+	if (pid == 0)
 	{
-		close(fd[0]);
-		write(fd[1], "arroz", 5);
+		char *buffer;
+		buffer = malloc(6);
 		close(fd[1]);
+		dup2(fdoutfile, 1);
+		read(fd[0], buffer, 6);
+		printf("bufer: %s", buffer);
+		close(fd[0]);
 	}
 	else
 	{
-		close(fd[1]);
-		char *buffer;
-		buffer = malloc(5);
-		read(fd[0], buffer, 5);
 		close(fd[0]);
-		printf("%s", buffer);
+		char *buffer;
+		buffer = malloc(6);
+		dup2(fdinfile, 0);
+		read(0, buffer, 6);
+		write(fd[1], buffer, 6);
+		close(fd[1]);
+		// close(fd[0]);
+		// write(fd[1], "arroz", 4);
+		// close(fd[1]);
 	}
 }
 
