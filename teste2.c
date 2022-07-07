e750496911ab86741df52530f145b44287c611f6
Vitor Sergio <vitorsergio111@gmail.com>
2022-07-06 22:42:03 -0300

    fixed teste2.c

--- teste2.c
+++ teste2.c
@@ -7,29 +7,29 @@ int main(int argc, char *argv[])
 {
 	int fdinfile = open("infile.txt", O_RDONLY, 0777);
 	int fdoutfile = open("outfile.txt", O_WRONLY, 0777);
-	int fd[2];
-	pipe(fd);
+	int readWrite[2];
+	pipe(readWrite);
 	int pid = fork();
 	
 	if (pid == 0)
 	{
 		char *buffer;
 		buffer = malloc(6);
-		close(fd[1]);
+		close(readWrite[1]);
 		dup2(fdoutfile, 1);
-		read(fd[0], buffer, 6);
+		read(readWrite[0], buffer, 6);
 		write(1, buffer, 6);
-		close(fd[0]);
+		close(readWrite[0]);
 	}
 	else
 	{
-		close(fd[0]);
+		close(readWrite[0]);
 		char *buffer;
 		buffer = malloc(6);
 		dup2(fdinfile, 0);
 		read(0, buffer, 6);
-		write(fd[1], buffer, 6);
-		close(fd[1]);
+		write(readWrite[1], buffer, 6);
+		close(readWrite[1]);
 	}
 }
 
