7e9406d6d8b9c09df7719ab8dd573b78fb78c54c
Vitor Sergio <vitorsergio111@gmail.com>
2022-07-15 23:41:58 -0300

    fixed fork

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 13:12:28 by vsergio           #+#    #+#             */
-/*   Updated: 2022/07/15 18:41:52 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/07/15 23:39:12 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,38 +42,27 @@ int	main(int argc, char *argv[], char **envp)
 void	firstchild(char **argv, int *readwrite, char **envp)
 {
 	int	fdinfile;
-	int	pid;
 
 	fdinfile = open(argv[1], O_RDONLY);
 	if (fdinfile == -1)
 		error_msg("Failed to open infile!", 2);
-	else if (pid == 0)
-	{
-		close(readwrite[0]);
-		dup2(fdinfile, 0);
-		dup2(readwrite[1], 1);
-		pathfilter(argv[2], envp);
-	}
+	close(readwrite[0]);
+	dup2(fdinfile, 0);
+	dup2(readwrite[1], 1);
+	pathfilter(argv[2], envp);
 }
 
 void	secondchild(char **argv, int *readwrite, char **envp)
 {
 	int	fdoutfile;
-	int	pid2;
 
 	fdoutfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fdoutfile == -1)
 		error_msg("Failed to open outfile!", 2);
-	pid2 = fork();
-	if (pid2 == -1)
-		error_msg("Failed to do second fork!", 10);
-	else if (pid2 == 0)
-	{
-		close(readwrite[1]);
-		dup2(readwrite[0], 0);
-		dup2(fdoutfile, 1);
-		pathfilter(argv[3], envp);
-	}
+	close(readwrite[1]);
+	dup2(readwrite[0], 0);
+	dup2(fdoutfile, 1);
+	pathfilter(argv[3], envp);
 }
 
 void	pathfilter(char *argv, char **envp)
