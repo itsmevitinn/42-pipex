d9d588e9a75bbd752681b024d69f7b68871fe97f
Vitor Marcondes <vitorsergio111@gmail.com>
2022-07-26 13:42:48 -0300

    changed gnl fd to stdin

--- pipex_utils2_bonus.c
+++ pipex_utils2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vsergio <vsergio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 04:24:43 by vsergio           #+#    #+#             */
-/*   Updated: 2022/07/26 12:00:55 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/07/26 13:41:13 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,28 @@ void	here_doc(char **argv, int **readwrite)
 {
 	char	*line;
 	char	*check;
-	int		fdheredoc;
 	int		fdtemp;
-	int		fdout;
+	int		fdinfile;
 
-	fdheredoc = open("here_doc", O_RDONLY);
-	fdtemp = open("tempfile.txt", O_WRONLY | O_CREAT | O_APPEND, 0666);
-	if (fdheredoc == -1 || fdtemp == -1)
-		error_msg("Failed to open file!", 1);
-	line = get_next_line(fdheredoc);
+	fdtemp = open("tempfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fdtemp == -1)
+		error_msg("tempfile.txt", 1);
+	write(1, "heredoc> ", 9);
+	line = get_next_line(STDIN_FILENO);
 	check = ft_strtrim(line, "\n");
 	while (ft_strncmp(check, argv[2], ft_strlen(check)) != 0 && line != NULL)
 	{
 		free(check);
 		write(fdtemp, line, ft_strlen(line));
 		free(line);
-		line = get_next_line(fdheredoc);
+		write(1, "heredoc> ", 9);
+		line = get_next_line(STDIN_FILENO);
 		check = ft_strtrim(line, "\n");
 	}
 	close(fdtemp);
-	fdout = open("tempfile.txt", O_RDONLY);
+	fdinfile = open("tempfile.txt", O_RDONLY);
 	unlink("tempfile.txt");
-	runcmds(argv[3], fdout, readwrite[0][1]);
+	runcmds(argv[3], fdinfile, readwrite[0][1]);
 }
 
 int	do_files(char *argv, int type)
